c1cba86f3d17887d7092387c5663789aaf313895
jdoan21 <jdoan21@stanford.edu>
2019-11-10 17:58:40 -0800

    more testing stuff and small fixes

--- mini_metro.py
+++ mini_metro.py
@@ -5,13 +5,13 @@ import enum
 LINES_LIMIT = 0.7 	# the fraction of number of total possible tracks that
 					# gives us the maximum number of lines we're allowed
 
-class Station(enum.Enum):
+class Station(int, enum.Enum):
 	Empty = 0
 	Triangle = 1
 	Circle = 2
 	Square = 3
 
-class Track(enum.Enum):
+class Track(int, enum.Enum):
 	NoTrack = 0
 	Track = 1
 
@@ -19,7 +19,7 @@ def enum_point(r, c, dim):
 	return r*(dim + 1) + c
 
 def de_enum_point(point, dim):
-	return int(point/(dim + 1)), point % (dim + 1)
+	return (int(point/(dim + 1)), (point % (dim + 1)))
 
 class MiniMetroGame:
 	def __init__(self, dim=3, n_types=2):
@@ -36,13 +36,13 @@ class MiniMetroGame:
 		for r in range(self.dim + 1):
 			for c in range(self.dim + 1):
 				cur = enum_point(r, c, self.dim)
-				self.stations[cur] = Station.Empty
+				self.stations[cur] = int(Station.Empty)
 				if c + 1 != self.dim + 1:
 					curBelow = enum_point(r, c + 1, self.dim)
-					self.tracks[(cur, curBelow)] = (Track.NoTrack, 0) # second value refers to number of times that the track is used
+					self.tracks[(cur, curBelow)] = (int(Track.NoTrack), 0) # second value refers to number of times that the track is used
 				if r + 1 != self.dim + 1:
 					curRight = enum_point(r + 1, c, self.dim)
-					self.tracks[(cur, curRight)] = (Track.NoTrack, 0)
+					self.tracks[(cur, curRight)] = (int(Track.NoTrack), 0)
 		self.maxNumLines = int(LINES_LIMIT*len(self.tracks))
 		self.totalTracksUtilized = 0 # only increment for the first time we utilize a track
 		self.totalTracksCovered = 0 # this includes double counts if two lines (or even one line) use the same track along its path
@@ -50,12 +50,14 @@ class MiniMetroGame:
 	def addLine(self):
 		if len(self.lines) == self.maxNumLines:
 			raise Exception('addLine(): Reached maximum number of lines')
-		newLine = len(self.lines) + 1 # it's one indexed
+		newLine = len(self.lines) + 1 # LINES ARE ONE-INDEXED - sorry lol
 		self.lines[newLine] = set() # set of tracks
 
 	def addTrackToLine(self, line, point1, point2):
 		# TODO!!! - think about how we double count (or more) a track for the same line?
 		# it would not update the set of tracks in self.lines but it would increase self.totalTracksCovered
+		if len(self.lines) == 0:
+			raise Exception('addTrackToLine(): There are no current lines')
 		if line not in self.lines:
 			raise Exception('addTrackToLine(): Entered invalid line')
 		track = (point1, point2)
@@ -63,12 +65,12 @@ class MiniMetroGame:
 			track = (point2, point1)
 		if track not in self.tracks:
 			raise Exception('addTrackToLine(): Entered invalid track')
-		if self.tracks[track][0] == Track.Track:
-			self.tracks[track] = (Track.Track, self.tracks[track][1] + 1)
+		if self.tracks[track][0] == int(Track.Track):
+			self.tracks[track] = (int(Track.Track), self.tracks[track][1] + 1)
 			self.lines[line].add(track)
 			self.totalTracksCovered += 1
 		else:
-			self.tracks[track] = (Track.Track, 1) # if it's already a track, that's aight
+			self.tracks[track] = (int(Track.Track), 1) # if it's already a track, that's aight
 			self.lines[line].add(track)
 			self.totalTracksUtilized += 1
 			self.totalTracksCovered += 1
@@ -88,31 +90,30 @@ class MiniMetroGame:
 		elif track not in self.lines[line]:
 			raise Exception('removeTrackFromLine(): Tried to remove a track not in use by this line')
 		if self.tracks[track][1] == 1:
-			self.tracks[track] = (Track.NoTrack, 0)
+			self.tracks[track] = (int(Track.NoTrack), 0)
 			self.lines[line].remove(track)
 			self.totalTracksCovered -= 1
 			self.totalTracksUtilized -= 1
 		else:
-			self.tracks[track] = (Track.Track, self.tracks[track][1] - 1)
+			self.tracks[track] = (int(Track.Track), self.tracks[track][1] - 1)
 			# !!!! account for double counts here, maybe don't remove from set
 			self.lines[line].remove(track)
 			# !!!! account for double counts here, maybe don't remove from set
 			self.totalTracksCovered -= 1
 
-	def addStation(point, station_type):
-		if 1 > station_type or station_type > self.n_station_types or point not in self.stations:
+	def addStation(self, point, station_type):
+		if (1 > station_type) or (station_type > self.n_station_types) or point not in self.stations:
 			raise Exception('addStation(): Entered invalid station')
 		if self.stations[point] != Station.Empty:
 			raise Exception('addStation(): Station has already been added')
 		self.stations[point] = station_type
 		self.n_stations += 1
 		if station_type == Station.Triangle:
-			self.triangleStations.add(point)
+			self.triangleStations.add(point) # de_enum_point(point, self.dim)
 		elif station_type == Station.Circle:
 			self.circleStations.add(point)
 		elif station_type == Station.Square:
 			self.squareStations.add(point)
-
 
 	def constructLine(self, line):
 		# TODO!!! - basically update tracks by using a path searching algorithm
@@ -120,6 +121,7 @@ class MiniMetroGame:
 		# the shortest path covering all the tracks in the set (from self.lines)
 		# ANOTHER THING - remove (and/or add) tracks if it makes sense to and update
 		# all values accordingly
+		# update self.totalTracksCovered, self.totalTracksUtilized, self.lines, self.stations, etc.
 		print('TODO!!!')
 
 	def getDim(self):
--- run.py
+++ run.py
@@ -1,12 +1,44 @@
 import mini_metro
-from mini_metro import MiniMetroGame
+from mini_metro import MiniMetroGame, Station, Track
 
-def main():
-	newGame = MiniMetroGame(2, 2)
+def enum_point(r, c, dim):
+	return r*(dim + 1) + c
+
+def de_enum_point(point, dim):
+	return int(point/(dim + 1)), (point % (dim + 1))
+
+def tryThings():
+	newGame = MiniMetroGame(2, 2) # 2x2 GRID
 	dim, lines, tracks, stations, n_stations = newGame.getState()
 	maxNumLines = newGame.getMaxNumLines()
-	print(maxNumLines)
+	# print(maxNumLines)
 	# print(len(tracks))
 
+	newGame.addStation(enum_point(0, 0, dim), Station.Triangle)
+	newGame.addStation(enum_point(0, 2, dim), Station.Triangle)
+	newGame.addStation(enum_point(1, 1, dim), Station.Circle)
+	newGame.addLine()
+	newGame.addLine()
+	newGame.addTrackToLine(1, enum_point(0, 1, dim), enum_point(0, 0, dim))
+	newGame.addTrackToLine(1, enum_point(0, 1, dim), enum_point(1, 1, dim))
+	newGame.addTrackToLine(2, enum_point(0, 2, dim), enum_point(0, 1, dim))
+	newGame.addTrackToLine(2, enum_point(0, 1, dim), enum_point(1, 1, dim))
+	print('triangle stations:', newGame.getTriangleStations())
+	print('circle stations:', newGame.getCircleStations())
+	print('tracks:', newGame.getTracks())
+	print('num stations:', newGame.getNumStations())
+	print('tracks utilized:', newGame.getTotalTracksUtilized())
+	print('tracks covered:', newGame.getTotalTracksCovered())
+	newGame.removeTrackFromLine(1, enum_point(0, 1, dim), enum_point(0, 0, dim))
+	print('\n\nRemoved Track from (0, 0) to (0, 1)!\n\n')
+	print('tracks:', newGame.getTracks())
+	print('num stations:', newGame.getNumStations())
+	print('tracks utilized:', newGame.getTotalTracksUtilized())
+	print('tracks covered:', newGame.getTotalTracksCovered())
+
+def main():
+	print('do real stuff here')
+
 if __name__ == '__main__':
-	main()
+	tryThings()
+	# main()
